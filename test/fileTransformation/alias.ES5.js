5b6895cde733209a238714d76afc90e5738e8db5
kimamula <kenji.imamula@gmail.com>
2019-11-24 12:42:50 +0900

    remove ts-transfomer-enumerate import expression

--- test/fileTransformation/alias.ES5.js
+++ test/fileTransformation/alias.ES5.js
@@ -1,11 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var index_1 = require("../../index");
 ({});
 var Foo = { "foo": "foo" };
 console.log(Foo.foo);
 ({ "foo": "foo", "bar": "bar" }.bar);
 ({ "foo": "foo", "bar": "bar", "baz": "baz" });
+function enumerate() {
+    return '';
+}
+var a = enumerate();
 index_1.enumerate.toString();
 var MyClass = /** @class */ (function () {
     function MyClass() {
---
+++ test/fileTransformation/alias.ESNext.js
@@ -0,0 +1,15 @@
+({});
+const Foo = { "foo": "foo" };
+console.log(Foo.foo);
+({ "foo": "foo", "bar": "bar" }.bar);
+({ "foo": "foo", "bar": "bar", "baz": "baz" });
+function enumerate() {
+    return '';
+}
+const a = enumerate();
+e.toString();
+class MyClass {
+    enumerate() {
+        return {};
+    }
+}
